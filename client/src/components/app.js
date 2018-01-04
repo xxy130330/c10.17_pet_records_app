682d78a80e190f231a73ad356330b073a7af6550
sangwoo89118 <sangwoo89118@gmail.com>
2018-01-04 13:52:27 -0800

    Completed converting parent_page to react with input functionality

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -4,6 +4,7 @@ import LandingPage from "./landing_page/landing_page";
 // import LoginPage from './login_page/login_page';
 import PetList from "./pet_list/pet_list";
 import PetProfile from "./pet_profile/pet_profile";
+import ParentPage from "./parent_page/parent_page";
 
 import RecordItem from "./record_item/record_item";
 import PetData from "./pet_data";
@@ -16,6 +17,7 @@ const App = () => (
             <Route  path = '/pet-list/' component={(props)=> {return(<PetList data={PetData}{...props}/>)}}/>
             <Route exact path = '/pet-profile/:id' component={(props)=> {return(<PetProfile data={PetData}{...props}/>)}}/>
             <Route exact path = '/pet-profile/:petId/record-item/:recordId'  component={(props)=>{return <RecordItem data={PetData}{...props} />}}/>
+            <Route path = '/parent-page/' component={ParentPage}/>
             <Footer/>
         </div>
     </Router>
