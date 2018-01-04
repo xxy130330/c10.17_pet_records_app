11bb4de23da74d781b91b14ef412fa5dd339e34a
sangwoo89118 <sangwoo89118@gmail.com>
2018-01-04 14:31:56 -0800

    finished login-page functionality

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "./landing_page/landing_page";
-// import LoginPage from './login_page/login_page';
+import LoginPage from './login_page/login_page';
 import PetList from "./pet_list/pet_list";
 import PetProfile from "./pet_profile/pet_profile";
 import ParentPage from "./parent_page/parent_page";
@@ -15,6 +15,7 @@ const App = () => (
     <Router>
         <div>
             <Route exact path = '/' component={LandingPage}/>
+            <Route path = '/login-page/' component={LoginPage}/>
             <Route  path = '/pet-list/' component={(props)=> {return(<PetList data={PetData}{...props}/>)}}/>
             <Route exact path = '/pet-profile/:id' component={(props)=> {return(<PetProfile data={PetData}{...props}/>)}}/>
             <Route exact path = '/pet-profile/:petId/record-item/:recordId'  component={(props)=>{return <RecordItem data={PetData}{...props} />}}/>
