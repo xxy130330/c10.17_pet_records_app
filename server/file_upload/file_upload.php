bad8ed256f7722591797db54d95a94b27742142d
xxy130330 <yangxue0708@gmail.com>
2018-01-10 15:48:59 -0800

    creat folder if there is not one

--- server/file_upload/file_upload.php
+++ server/file_upload/file_upload.php
@@ -1,26 +1,23 @@
 <?php
 
 if(!empty($_POST) && !empty($_FILES)){
-    if(is_dir('owner_id_' . $_POST['ownerID'])) {
-        if(is_dir('pet_id_' . $_POST['petID'])) {
-            moveImage();
-            } else {
-                print('something went wrong!');
-            }
-        }else{
-            mkdir(" ./pet_id_{$_POST['petID']}/upload_image");
-            moveImage();
-        }
-    }else{
-        mkdir(" ./owner_id{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image");
-        moveImage();
+    if(!is_dir('owner_id_' . $_POST['ownerID'])) {
+        mkdir("./owner_id_{$_POST['ownerID']}");
+    }
+    if(!is_dir("./owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}")) {
+        mkdir("./owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}");
+    }
+    moveImage();
 }
 
+
 function moveImage(){
-    if (move_uploaded_file($_FILES['avatar']['tmp_name'], "../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image/{$_FILES['avatar']['name']}")) {
+    $pathInfo = pathinfo($_FILES['avatar']['name']);
+    $newName = $_FILES['avatar']['name'] . time() . $pathInfo['extension'];
+    if (move_uploaded_file($_FILES['avatar']['tmp_name'], "../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/{$newName}")) {
         print('Your file was uploaded.');
         ?>
-        <img src="../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image/<?= $_FILES['avatar']['name']; ?>">
+        <img src="../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/<?= $_FILES['avatar']['name']; ?>">
         <?php
     } else {
         print('something went wrong!');
