16acaf6e485fc88ba80783fe05cc6c9ebd9af8fb
xxy130330 <yangxue0708@gmail.com>
2018-01-10 14:06:03 -0800

    created if statement to check if the dir is exist, if not create one

--- server/file_upload/file_upload.php
+++ server/file_upload/file_upload.php
@@ -1,14 +1,19 @@
 <?php
 
 if(!empty($_POST) && !empty($_FILES)){
-    if(!folder_exists())
-    if(move_uploaded_file($_FILES['avatar']['tmp_name'], "../file_upload/owner_id_1/upload_image/{$_FILES['avatar']['name']}")){
-        print('Your file was uploaded.');
-        ?>
-        <img src="../file_upload/owner_id_1/upload_image/<?=$_FILES['avatar']['name'];?>">
-        <?php
-    }else{
-        print('something went wrong!');
+    if(is_dir('owner_id_' . $_POST['ownerID'])) {
+        if(is_dir('pet_id_' . $_POST['petID'])) {
+            if (move_uploaded_file($_FILES['avatar']['tmp_name'], "../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image/{$_FILES['avatar']['name']}")) {
+                print('Your file was uploaded.');
+                ?>
+                <img src="../file_upload/owner_id_1/upload_image/<?= $_FILES['avatar']['name']; ?>">
+                <?php
+            } else {
+                print('something went wrong!');
+            }
+        }else{
+            mkdir(" ./'owner_id_' {$_POST['ownerID']}/'pet_id_' {$_POST['petID']}/upload_image");
+        }
     }
 }
 
