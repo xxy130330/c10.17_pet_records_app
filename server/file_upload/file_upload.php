5445a8a5c21970e2e9fdeb3e75b3075b6c18f449
xxy130330 <yangxue0708@gmail.com>
2018-01-10 11:13:22 -0800

    created file upload file to contain php and image files

--- server/file_upload/file_upload.php
+++ server/file_upload/file_upload.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!empty($_POST) && !empty($_FILES)){
-    if(move_uploaded_file($_FILES['avatar']['tmp_name'], "../uploads/{$_FILES['avatar']['name']}")){ //将上传文件从临时存放处，转移到最终存放处
+    if(move_uploaded_file($_FILES['avatar']['tmp_name'], "../uploads/{$_FILES['avatar']['name']}")){
         print('Your file was uploaded.');
         ?>
         <img src="../uploads/<?=$_FILES['avatar']['name'];?>">
@@ -12,11 +12,10 @@ if(!empty($_POST) && !empty($_FILES)){
 }
 
 
+?>
 
 
-
-
-?>/**
+/**
  * Created by PhpStorm.
  * User: christin
  * Date: 1/9/18
