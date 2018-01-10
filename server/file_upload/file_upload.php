554ebde44702d3d08981812e01ebc260ef9cf363
xxy130330 <yangxue0708@gmail.com>
2018-01-10 14:25:07 -0800

    created function to move image

--- server/file_upload/file_upload.php
+++ server/file_upload/file_upload.php
@@ -3,27 +3,28 @@
 if(!empty($_POST) && !empty($_FILES)){
     if(is_dir('owner_id_' . $_POST['ownerID'])) {
         if(is_dir('pet_id_' . $_POST['petID'])) {
-            if (move_uploaded_file($_FILES['avatar']['tmp_name'], "../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image/{$_FILES['avatar']['name']}")) {
-                print('Your file was uploaded.');
-                ?>
-                <img src="../file_upload/owner_id_1/upload_image/<?= $_FILES['avatar']['name']; ?>">
-                <?php
+            moveImage();
             } else {
                 print('something went wrong!');
             }
         }else{
-            mkdir(" ./'owner_id_' {$_POST['ownerID']}/'pet_id_' {$_POST['petID']}/upload_image");
+            mkdir(" ./pet_id_{$_POST['petID']}/upload_image");
+            moveImage();
         }
+    }else{
+        mkdir(" ./owner_id{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image");
+        moveImage();
+}
+
+function moveImage(){
+    if (move_uploaded_file($_FILES['avatar']['tmp_name'], "../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image/{$_FILES['avatar']['name']}")) {
+        print('Your file was uploaded.');
+        ?>
+        <img src="../file_upload/owner_id_{$_POST['ownerID']}/pet_id_{$_POST['petID']}/upload_image/<?= $_FILES['avatar']['name']; ?>">
+        <?php
     }
 }
 
 
-?>
 
 
-/**
- * Created by PhpStorm.
- * User: christin
- * Date: 1/9/18
- * Time: 16:13
- */
