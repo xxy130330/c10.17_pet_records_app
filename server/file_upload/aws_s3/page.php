003c2781e57814793043ae50578b909a9fe1590c
xxy130330 <yangxue0708@gmail.com>
2018-01-11 14:06:00 -0800

    created connectDB file to connect to db

--- server/file_upload/aws_s3/page.php
+++ server/file_upload/aws_s3/page.php
@@ -1,4 +1,3 @@
-<?php header('Access-Control-Allow-Origin: *'); ?>
 
 
 <?php
@@ -42,42 +41,6 @@ if(isset($_POST['upload'])){
 
 
 
-if(empty($_GET['course'])){
-    $field = 'name, grade, course';
-    $whereClause = '';
-}else{
-    $field = '*';
-    $whereClause = "WHERE course = {$_GET['course']}";
-}
 
-$query = "INSERT INTO students SET 
-          name = '{$_GET['name']}',
-          course = '{$_GET['course']}',
-          grade = '{$_GET['grade']}'
-          ";
-
-$result = mysqli_query($conn, $query); //store as an object; $query is like command line runs in sql; $conn is like a key to database($conn)
-
-$output = [
-    'success' => false,
-    'data' => [],
-    'errors' => []
-];
-
-if($result){
-    if(mysqli_affected_rows($conn)>0){ //return num of rows
-        $output['success'] = true;
-        $output['data'] = mysqli_insert_id($conn);
-    }else{
-        $output['errors'][] = 'unable to insert data';
-    }
-}else{
-    $output['errors'][] = 'error in SQL query';
-}
-
-
-$json_output = json_encode($output); //convert $output into json(object)
-
-print($json_output);
 
 ?>
