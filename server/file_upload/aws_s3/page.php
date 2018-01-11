dd08ffcea75898ca9b4d0e67e7ad472fc1ccb8e9
xxy130330 <yangxue0708@gmail.com>
2018-01-11 11:34:03 -0800

    created an object to store url according with ownerID

--- server/file_upload/aws_s3/page.php
+++ server/file_upload/aws_s3/page.php
@@ -4,6 +4,7 @@ if (!class_exists('S3'))require_once('S3.php');
 
 //AWS access info
 require_once('credential.php');
+
 if (!defined('awsAccessKey')) define('awsAccessKey', $accessKey);
 if (!defined('awsSecretKey')) define('awsSecretKey', $secretKey);
 
@@ -14,29 +15,26 @@ $s3 = new S3(awsAccessKey, awsSecretKey);
 //check whether a form was submitted
 if(isset($_POST['Submit'])){
 
-    $fileName = $_FILES['theFile']['name']; //rename the file name by adding current timestamp
+    $fileName = time() . $_FILES['theFile']['name']; //rename the file name by adding current timestamp
     $fileTempName = $_FILES['theFile']['tmp_name'];
 
-//create a new bucket
-    $s3->putBucket("image_folder", S3::ACL_PUBLIC_READ);
+//create a new
+    $bucketName = 'petvet_files';
+    $s3->putBucket($bucketName, S3::ACL_PUBLIC_READ);
 
 //move the file
-if ($s3->putObjectFile($fileTempName, "image_folder", $fileName, S3::ACL_PUBLIC_READ)) {
-    echo "We successfully uploaded your file.";
-}else{
-    echo "Something went wrong while uploading your file... sorry.";
-}}
+    if ($s3->putObjectFile($fileTempName, $bucketName, $fileName, S3::ACL_PUBLIC_READ)) {
+        echo "We successfully uploaded your file.";
+    }else{
+        echo "Something went wrong while uploading your file... sorry.";
+    }
+}
 
+//put url into object
+$urlObj = (object)array(
+    $_POST['ownerID'] => "http://{$bucketName}.s3.amazoneaws.com/{$fileName}"
+)
 
-//$contents = $s3->getBucket("image_folder");
-//foreach ($contents as $file){
-//
-//    $fname = $file['name'];
-//    $furl = "http://image_folder.s3.amazonaws.com/{$fname}";
-//
-//    //output a link to the file
-//    echo "<a href=\"$furl\">$fname</a><br />";
-//}
 
 ?>
 /**
