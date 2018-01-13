18354b95eeb032a640dda9d0552520757276a942
camposss <ccamposs23@gmail.com>
2018-01-13 13:29:33 -0800

    simon helped tweaking the php files responsible for deleting pet and medical record items

--- server/database_connect/actions/soft_delete_pet.php
+++ server/database_connect/actions/soft_delete_pet.php
@@ -24,23 +24,23 @@ if ($result) {
         $output['errors'][] = 'no data available';
     }
 } else {
-    $output['errors'][] = 'Error in SQL Query';
+    $output['errors'][] = 'Error in SQL Query 404';
 }
 
-$query = "UPDATE `medical_records` SET `status` = 'inactive', `updated` = CURRENT_DATE WHERE `petID` = $ID";
-
-$result = mysqli_query($conn, $query);
-
-if ($result) {
-    if (mysqli_affected_rows($conn) > 0) {
-        $output['success'] = true;
-        $output['data'][] = 'The records is set to inactive';
-    } else {
-        $output['errors'][] = 'no record items to inactivate';
-    }
-} else {
-    $output['errors'][] = 'Error in SQL Query';
-}
+//$query = "UPDATE `medical_records` SET `status` = 'inactive', `updated` = CURRENT_DATE WHERE `petID` = $ID";
+//
+//$result = mysqli_query($conn, $query);
+//
+//if ($result) {
+//    if (mysqli_affected_rows($conn) > 0) {
+//        $output['success'] = true;
+//        $output['data'][] = 'The records is set to inactive';
+//    } else {
+//        $output['errors'][] = 'no record items to inactivate';
+//    }
+//} else {
+//    $output['errors'][] = 'Error in SQL Query';
+//}
 
 
 ?>
