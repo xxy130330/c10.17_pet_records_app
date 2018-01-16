e7b1f2fa721ac5cbdf2d1a8fa0e68ca118a4d456
camposss <ccamposs23@gmail.com>
2018-01-16 10:12:13 -0800

    got ternary working upon clicking edit button. need to make post axios call to update server

--- client/src/components/record_item/record_item.js
+++ client/src/components/record_item/record_item.js
@@ -8,35 +8,52 @@ class RecordItem extends Component {
 
     constructor(props){
         super(props);
-
-        this.canEdit= false;
+        this.state={
+            canEdit: false
+        }
     }
 
   componentWillMount() {
     this.props.fetchMedicalData(this.props.match.params.recordId);
     console.log('record id using the url, ', this.props.match.params.recordId);
-    console.log('can we edit upon mounting? ', this.canEdit);
+    console.log('can we edit upon mounting? ', this.state.canEdit);
   }
 
   handleEditClick(){
-        this.canEdit=true;
-        console.log('the handle edit button has been clicked', this.canEdit);
+      this.setState({
+            canEdit: true
+        });
+      console.log('the handle edit button has been clicked', this.state.canEdit);
+
+  }
+  renderButton(){
+      if(this.state.canEdit){
+          return <button onClick={()=>this.saveChanges()} className='btn btn-success'>Save Changes</button>
+      }
+  }
+  saveChanges(){
+      console.log('we want to save the changes!');
+      this.setState({
+          canEdit: false
+      });
 
   }
   render() {
     if (!this.props.petMedical.length) {
       return <h1>Loading</h1>;
     }
+      // console.log('the handle edit button has been clicked', this.state.canEdit);
+      return (
 
-    return (
         <div className=" record_item_container">
             <div>
                 <button onClick={()=>this.handleEditClick()} className='btn btn-warning'>Edit</button>
             </div>
-            <h2 className="record_item_header" contentEditable={this.canEdit? 'true': 'false'}>{this.props.petMedical[0].type}</h2>
-            <h3 className="record_item_date" contentEditable={this.canEdit? 'true': 'false'}>{this.props.petMedical[0].date}</h3>
+            <h2 className="record_item_header" contentEditable={this.state.canEdit? 'true': 'false'}>{this.props.petMedical[0].type}</h2>
+            <h3 className="record_item_date" contentEditable={this.state.canEdit? 'true': 'false'}>{this.props.petMedical[0].date}</h3>
             <hr />
-            <p contentEditable={this.canEdit? 'true': 'false'}>{this.props.petMedical[0].details}</p>
+            <p contentEditable={this.state.canEdit? 'true': 'false'}>{this.props.petMedical[0].details}</p>
+            {this.state.canEdit? this.renderButton(): ''}
         </div>
     );
   }
