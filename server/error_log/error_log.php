6f3ae01c35129cf3c95d9f1f548c3ab8edd2fd96
xxy130330 <yangxue0708@gmail.com>
2018-01-20 13:34:14 -0800

    switch branch

--- server/error_log/error_log.php
+++ server/error_log/error_log.php
@@ -1,9 +1,42 @@
 <?php
+require_once("config.php");
+set_time_limit(0);
+header("Content-type:text/html;charset=utf-8");
+if($_SERVER['REMOTE_ADDR'] != '52.53.120.129') {
+    header('HTTP 1.1/404');
+}
+$line = 40;
+if(function_exists('ini_get')) {
+    $error_log = ini_get('error_log');
+} else {
+    $error_log = "/var/www/log/php_error.log";
+}
+$starttime = time();
+readlog($error_log, $line, $starttime);
 
-
-phpinfo();
-
-
+function readlog($file, $linefromlast, $starttime){
+    $i = 0;
+    $line = 0;
+    $fp = @fopen($file, "r");
+    while(stream_get_line($fp, 8192, "n")) {
+        $line++;
+    }
+    fseek($fp, 0);
+    if($fp) {
+        while(!feof($fp)) {
+            $i++;
+            $buffer = fgets($fp, 8192);
+            if($i <= ($line - $linefromlast)) {
+                continue;
+            }
+            echo $buffer . "<br />";
+        }
+        fclose($fp);
+    }
+    $endtime = time();
+    $havetime = $endtime - $starttime;
+    echo "In" . $line . "find out the latest record" . $linefromlast . "timing" . $havetime . "second";
+}
 ?>
 /**
  * Created by PhpStorm.
