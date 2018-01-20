55b6c6793ab5389923db22d4d5262dc45d4ebd16
Shoblik <shoblik@yahoo.com>
2018-01-19 16:41:55 -0800

    email verification link now gets sent to vets upon registering for account activation

--- server/database_connect/actions/update_activate_account.php
+++ server/database_connect/actions/update_activate_account.php
@@ -9,6 +9,7 @@ if(!isset($PAGEACCESS) || $PAGEACCESS===false){
     die('NO DIRECT ACCESS ALLOWED');
 }
 $activation_code = $_GET['actNum'];
+$output['data'][] = $activation_code;
 
 //grab the users ID base on activation code
 $query = "SELECT `ID` FROM `activation` WHERE `activation_code` = '$activation_code'";
@@ -18,22 +19,31 @@ if ($result) {
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $personID = $row['ID'];
-            $IdLength = count($personID);
+            $IdLength = strlen($personID);
 
             //if its a vet
-            if ($IdLength == 6) {
-                $query = "UPDATE `vets` SET `status` = 'active' WHERE `ref_ID` = $personID";
+            if ($IdLength === 6) {
+                $query = "UPDATE `vets` SET `status` = 'active' WHERE `ref_ID` = '$personID'";
                 $result = mysqli_query($conn, $query);
-                if ($result) {
-                    if (mysqli_affected_rows($conn) > 0) {
-                        $output['success'] = true;
-                    } else {
-                        $output['errors'][] = 'no vet found with that referenceID';
-                    }
-                } else {
-                    $output['errors'][] = 'Error in SQL query updating vet status';
-                }
+            } else {
+                //if its an owner
+                $query = "UPDATE `owner` SET `status` = 'active' WHERE `ID` = '$personID'";
+                $result = mysqli_query($conn, $query);
             }
+            $output['data'][] = $query;
+            $output['data'][] = $IdLength;
+
+
+            if ($result) {
+                if (mysqli_affected_rows($conn) > 0) {
+                    $output['success'] = true;
+                } else {
+                    $output['errors'][] = 'no info found with that referenceID';
+                }
+            } else {
+                $output['errors'][] = 'Error in SQL query updating vet/owner status';
+            }
+
         }
     } else {
         $output['errors'][] = 'That activation code doesn\'t exist';
