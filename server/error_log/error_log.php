ec7cef91c1e1d6801206bd451eabcd0445a830e6
xxy130330 <yangxue0708@gmail.com>
2018-01-22 10:15:10 -0800

    added comments to code

--- server/error_log/error_log.php
+++ server/error_log/error_log.php
@@ -2,14 +2,14 @@
 require_once("config.php");
 set_time_limit(0);
 header("Content-type:text/html;charset=utf-8");
-if($_SERVER['REMOTE_ADDR'] != '52.53.120.129') {
+if($_SERVER['REMOTE_ADDR'] != '52.53.120.129'){   //petvet server IP address
     header('HTTP 1.1/404');
 }
 $line = 40;
 if(function_exists('ini_get')) {
     $error_log = ini_get('error_log');
 } else {
-    $error_log = "/var/www/log/php_error.log";
+    $error_log = "/var/www/log/php_error.log"; //log location
 }
 $starttime = time();
 readlog($error_log, $line, $starttime);
@@ -38,6 +38,8 @@ function readlog($file, $linefromlast, $starttime){
     echo "In" . $line . "find out the latest record" . $linefromlast . "timing" . $havetime . "second";
 }
 ?>
+
+
 /**
  * Created by PhpStorm.
  * User: christin
