b072da4aba33ffcae56aaa7d245096ec238592f2
Shoblik <shoblik@yahoo.com>
2018-01-22 16:42:51 -0800

    re rendering the pet profile data once the user click Disconnect Pet

--- client/src/components/pet_profile/pet_profile.js
+++ client/src/components/pet_profile/pet_profile.js
@@ -56,9 +56,7 @@ class PetProfile extends Component {
                 var vetName= petDataArray[i].vet;
             }
         }
-        console.log('ownerID: ', ownerId );
-        console.log('petID: ', petId);
-        console.log('vetName: ', vetName);
+
         const url = '/server/database_connect/server.php?action=post&resource=disconnectPet';
         axios({
             method: 'post',
@@ -69,9 +67,17 @@ class PetProfile extends Component {
                 petID: petId,
                 ownerID: ownerId,
             }
-        }).then(function(res) {
-            console.log(res);
-        })
+        }).then(res => {
+            let currentOwnerId = null;
+            if(this.props.id){
+                currentOwnerId = this.props.id;
+                localStorage.id = currentOwnerId;
+            } else {
+                currentOwnerId = localStorage.id;
+            }
+            this.props.fetchProfileData(this.props.match.params.id)
+                .then(()=>this.props.fetchPetData(currentOwnerId));
+        });
 
     }
   getPetInfo() {
