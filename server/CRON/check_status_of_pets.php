78f244b42c54b516bc868dc75a69d76e4f1f804d
root <root@ip-172-31-7-63.us-west-1.compute.internal>
2018-01-23 03:45:12 +0000

    merging

--- server/CRON/check_status_of_pets.php
+++ server/CRON/check_status_of_pets.php
@@ -11,28 +11,20 @@ require_once ('/var/www/petvet.tech/server/database_connect/connect.php');
 $query = "DELETE FROM pets WHERE status='inactive' AND updated < DATE_SUB(NOW(), INTERVAL 1 YEAR)";
 
 $result = mysqli_query($conn, $query);
-print_r($conn);
-$output = [
-    'success' => false,
-    'data' => [],
-    'errors' => []
-];
 
 if($result){
     if(mysqli_affected_rows($conn)>0){
-        $output['success'] = true;
-	$output['rowsDeleted'] = mysqli_affected_rows($conn);
+	$message = date('Y-m-d H:i:s') . ': deleted '. mysqli_affected_rows($conn).' rows';
     }else{
-        $output['errors'][] = 'no rows deleted';
+	$message = date('Y-m-d H:i:s') . ': no rows deleted';
     }
+
 }else{
-    $output['errors'][] = 'error in SQL query';
+    $message = 'error in SQL query';
 }
 
+print($message);
 
-$json_output = json_encode($output);
-
-print($json_output);
 
 
 
