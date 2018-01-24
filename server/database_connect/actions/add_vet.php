0b22746c531a79d825ced782313e4f9ad243819d
xxy130330 <yangxue0708@gmail.com>
2018-01-23 18:32:28 -0800

    created function to check if refID is unique

--- server/database_connect/actions/add_vet.php
+++ server/database_connect/actions/add_vet.php
@@ -4,6 +4,18 @@ if(!isset($PAGEACCESS) || $PAGEACCESS===false){
     die('NO DIRECT ACCESS ALLOWED');
 }
 
+
+
+function getRandomRefID(){
+    do{
+        $refId = uniqid();
+        $checkQuery = "SELECT * FROM vets WHERE `ref_ID` = '$refId'";
+        $result = mysqli_query($conn, $checkQuery);
+    }while( mysqli_num_rows($result)>0);
+
+    return $refId;
+}
+
 $queryCheckEmail = "SELECT `email` FROM `vets` WHERE `email` = '$post[email]'";
 
 $checkResult = mysqli_query($conn, $queryCheckEmail);
@@ -11,30 +23,12 @@ $checkResult = mysqli_query($conn, $queryCheckEmail);
 //Check to see if the email is already in the db
 if ($checkResult) {
     if (mysqli_num_rows($checkResult) === 0) {
-        $query = "INSERT INTO `vets` (`name`, `email`, `phone`, `ID`, `ref_ID`, `active_pets`, `password`, `status`, `updated`, `level`) VALUES ('$post[name]', '$post[email]', '$post[phone]', NULL, '0', 'NULL', SHA1('$post[password]'), 'inactive', CURRENT_DATE, '2')";
+        $ref_id = getRandomRefID();
+        $query = "INSERT INTO `vets` (`name`, `email`, `phone`, `ID`, `ref_ID`, `active_pets`, `password`, `status`, `updated`, `level`) VALUES ('$post[name]', '$post[email]', '$post[phone]', NULL, '$ref_id', 'NULL', SHA1('$post[password]'), 'inactive', CURRENT_DATE, '2')";
         $result = mysqli_query($conn, $query);
         if ($result) {
             if (mysqli_affected_rows($conn) > 0) {
-                $resultID = mysqli_insert_id($conn);
-                $ref_ID = substr(MD5($resultID),0,6);
-
-                $checkQuery = "SELECT * FROM vets WHERE `ref_ID` = $ref_ID";
-                $result = mysqli_query($conn, $checkQuery);
-                if($result) {
-                    if (mysqli_num_rows($result) > 0) {
-                        $ref_ID = substr(MD5($resultID), 0, 6);
-                    }
-                    $output['data'][] = $ref_ID;
-                    $refIDQuery = "UPDATE `vets` SET `ref_ID` = '$ref_ID' WHERE `ID` = $resultID";
-
-                    $output['query'] = $refIDQuery;
-                    $result = mysqli_query($conn, $refIDQuery);
-                    $output['ref_ID'] = $ref_ID;
-                    if ($result) {
-                        if (mysqli_affected_rows($conn) > 0) {
-                            $output['success'] = true;
-                        }
-                    }
+                    //success!
                 }else {
                     $output['errors'][] = 'no data available';
                 }
@@ -66,19 +60,19 @@ if ($checkResult) {
                     $output['errors'][] = 'Error in SQL query inserting into activation';
                     $output['success'] = false;
                 }
-            } else {
-                $output['errors'][] = 'no data available';
+//            } else {
+//                $output['errors'][] = 'no data available';
+//            }
             }
-        }
-        else {
-            $output['errors'][] = 'Error in SQL query, inserting user22';
+//        else {
+//            $output['errors'][] = 'Error in SQL query, inserting user22';
+//        }
+        } else {
+            $output['errors'][] = 'That email is already in use';
         }
     } else {
-        $output['errors'][] = 'That email is already in use';
+        $output['errors'][] = 'Error in SQL query, checking if the email exists';
+        $output['errors'][] = $queryCheckEmail;
     }
-} else {
-    $output['errors'][] = 'Error in SQL query, checking if the email exists';
-    $output['errors'][] = $queryCheckEmail;
-}
 
 ?>
