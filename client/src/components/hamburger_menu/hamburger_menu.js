4ef98e5c0bcd1f2b9d3507f5238b0c0880b0c009
camposss <ccamposs23@gmail.com>
2018-01-23 16:14:46 -0800

    made link to client list in hamburger menu dynamic using redux state

--- client/src/components/hamburger_menu/hamburger_menu.js
+++ client/src/components/hamburger_menu/hamburger_menu.js
@@ -79,7 +79,7 @@ class NavBar extends Component {
               <div> Pet List </div>
             </Link>
           ) : (
-            <Link to="/client-list/:vetId" onClick={this.handleOnClick}>
+            <Link to={`/client-list/${this.props.vetId}`} onClick={this.handleOnClick}>
               <div> Client List </div>
             </Link>
           )}
@@ -127,7 +127,8 @@ class NavBar extends Component {
 function mapStateToProps(state) {
   return {
     auth: state.user.auth,
-    vetAccessLevel: state.vetlogin.accessLevel
+    vetAccessLevel: state.vetlogin.accessLevel,
+    vetId: state.vetlogin.id
   };
 }
 
