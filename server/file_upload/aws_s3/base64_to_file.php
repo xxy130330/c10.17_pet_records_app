18469525b3b907dc8a9ae69fa4bfd57fab69357c
Shoblik <shoblik@yahoo.com>
2018-01-24 17:41:43 -0800

    I just got the img upload working in base64!

--- server/file_upload/aws_s3/base64_to_file.php
+++ server/file_upload/aws_s3/base64_to_file.php
@@ -1,24 +1,49 @@
 <?php
-
+/**
+ * Created by PhpStorm.
+ * User: shobl
+ * Date: 1/24/2018
+ * Time: 5:11 PM
+ */
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-if(!isset($PAGEACCESS) || $PAGEACCESS===false){
-    die('NO DIRECT ACCESS ALLOWED');
-}
+
+//if(!isset($PAGEACCESS) || $PAGEACCESS===false){
+//    die('NO DIRECT ACCESS ALLOWED');
+//}
+
 if (!class_exists('S3'))require_once('s3.php');
+
 require_once('credential.php');
 //require_once('../../database_connect/connect.php');
+
+
 if (!defined('awsAccessKey')) define('awsAccessKey', $accessKey);
 if (!defined('awsSecretKey')) define('awsSecretKey', $secretKey);
+
+
+$postJSON = file_get_contents('php://input');
+$post = json_decode($postJSON, TRUE);
+
 $s3 = new S3(awsAccessKey, awsSecretKey);
-//print_r($_POST);
+
 $bucketName = 'petvetlfz';
-//$jsondata = file_get_contents("php://input");
-//$_POST = json_decode($jsondata, true);
-//print_r($_POST);
-$fileName = time() . $_FILES['file']['name'];
+$output['data'][] = $post['formData'];
+
+
+define('UPLOAD_DIR', './images/');
+$img = $post['rawData'];
+$imgHead = substr($img, 0, 22);
+$img = str_replace($imgHead, '', $img);
+$img = str_replace(' ', '+', $img);
+$data = base64_decode($img);
+$file = UPLOAD_DIR . uniqid() . '.png';
+$success = file_put_contents($file, $data);
+print $success ? $file : 'Unable to save the file.';
+
+$fileName = time() . $file;
 //if(isset($_POST['upload'])){
-$fileTempName = $_FILES['file']['tmp_name'];
+$fileTempName = $file;
 if(!isset($bucketName)){
     $s3->putBucket($bucketName, S3::ACL_PUBLIC_READ);
 }
@@ -28,12 +53,6 @@ if ($s3->putObjectFile($fileTempName, $bucketName, $fileName, S3::ACL_PUBLIC_REA
 }else{
 //        echo "Something went wrong while uploading your file... sorry.";
 };
-//}
-//$output['data'][] = $url;
-//$output['errors'][] = $url;
-//require_once ('./credential.php');
-//require_once ('../../file_upload/update_avatar_link_db.php');
-$url = "http://{$bucketName}.s3.amazonaws.com/".$fileName;
-$output['data'][] = $url;
 
-?>
+    print($url = "http://{$bucketName}.s3.amazonaws.com/".$fileName);
+
