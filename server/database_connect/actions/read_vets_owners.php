e732678960db7eebd72d8ddd3a83600ad99acc1a
Shoblik <shoblik@yahoo.com>
2018-01-25 21:31:22 -0800

    cleaned up read_vets_owners.php

--- server/database_connect/actions/read_vets_owners.php
+++ server/database_connect/actions/read_vets_owners.php
@@ -9,13 +9,13 @@
 if(!isset($PAGEACCESS) || $PAGEACCESS===false){
     die('NO DIRECT ACCESS ALLOWED');
 }
-
 $vetID = $_GET['vetID'];
 $ownerArray = array();
 
 //pull active_pets from vets table
-
-$query = "SELECT `active_pets`, `email`, `ref_ID` FROM `vets` WHERE `ID` = $vetID";
+$query = "SELECT `active_pets`, `email`, `ref_ID` 
+          FROM `vets` 
+          WHERE `ID` = $vetID";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
@@ -31,10 +31,10 @@ if ($result) {
             }
 
             //loop over the ownerID's in the $ownerArray
-            //perform an axios call to fetch the owner data associated with that ownerID
-
             for ($k = 0; $k < $count; $k++) {
-                $query = "SELECT `ID` AS `ownerID`, `name`, `email` FROM `owner` WHERE `ID` = $ownerArray[$k]";
+                $query = "SELECT `ID` AS `ownerID`, `name`, `email` 
+                          FROM `owner` 
+                          WHERE `ID` = $ownerArray[$k]";
                 $result = mysqli_query($conn, $query);
 
                 if ($result) {
@@ -42,21 +42,11 @@ if ($result) {
                         while ($row = mysqli_fetch_assoc($result)) {
                             $output['data'][] = $row;
                         }
-                    } else {
-                        $output['errors'][] = 'couldn\'t find an owner with that ID';
-                    }
-                } else {
-                    $output['errors'][] = 'Error in SQL query fetching owner data';
-                }
+                    } else {$output['errors'][] = 'no data found';}
+                } else {$output['errors'][] = 'error in query';}
             }
-
-
         }
-    } else {
-        $output['errors'][] = 'That vet ID doesn\'t exist';
-    }
-} else {
-    $output['errors'][] = 'Error in SQL query fetching vet data';
-}
+    } else {$output['errors'][] = 'no data available';}
+} else {$output['errors'][] = 'error in query';}
 
 ?>
