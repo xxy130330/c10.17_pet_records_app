9fc45124b4aa3b3eb9f354a138e14afc870a150c
Shoblik <shoblik@yahoo.com>
2018-01-25 22:00:44 -0800

    cleaned up fetch_record_items.php

--- server/database_connect/actions/fetch_record_items.php
+++ server/database_connect/actions/fetch_record_items.php
@@ -9,33 +9,20 @@
 if(!isset($PAGEACCESS) || $PAGEACCESS===false){
     die('NO DIRECT ACCESS ALLOWED');
 }
-
 $ID = $_GET['petID'];
 
-
-
 $query = "SELECT `title` AS `type`, `ID` AS `recordID` 
-FROM medical_records 
-WHERE petID = $ID 
-AND `status` = 'active'";
-
-
-//Grab DOB and breed
-
+          FROM medical_records 
+          WHERE petID = $ID 
+          AND `status` = 'active'";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
     $output['success'] = true;
-
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $pet_objects[] = $row;
         }
-    } else {
-        $output['errors'][] = 'no data available';
-    }
-}
-else {
-    $output['errors'][] = 'error in SQL query 22';
-}
+    } else {$output['errors'][] = 'no data available';}
+} else {$output['errors'][] = 'error in query';}
 
