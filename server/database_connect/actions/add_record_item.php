24f20a9df08fa788299df956f10013c90dc67e90
Shoblik <shoblik@yahoo.com>
2018-01-25 22:21:52 -0800

    cleaned up add_record_item.php

--- server/database_connect/actions/add_record_item.php
+++ server/database_connect/actions/add_record_item.php
@@ -10,31 +10,22 @@ if(!isset($PAGEACCESS) || $PAGEACCESS===false){
     die('NO DIRECT ACCESS ALLOWED');
 }
 
-$output['test'][] = 'yo ho';
 $ID = $post['petID'];
 $title = $post['title'];
 $type = $post['type'];
 $recordData = $post['record_data'];
 $date = $post['treatment_date'];
 
-$query = "INSERT INTO `medical_records` SET `title` = ' $title ', `type` = ' $type ', `record_data` = ' $recordData ', `treatment_date` = ' $date ', `petID` = ' $ID ', `status` = 'active', `updated` = CURRENT_DATE";
-
+$query = "INSERT INTO `medical_records` 
+          SET `title` = ' $title ', `type` = ' $type ', `record_data` = ' $recordData ', `treatment_date` = ' $date ', `petID` = ' $ID ', `status` = 'active', `updated` = CURRENT_DATE";
 $result = mysqli_query($conn, $query);
-
-$output['errors'][] = $query;
-
-
 
 if ($result) {
     if (mysqli_affected_rows($conn) > 0) {
         $output['success'] = true;
-    } else {
-        $output['errors'][] = 'no data available';
-    }
+    } else {$output['errors'][] = 'no data available';}
 }
-else {
-    $output['errors'][] = 'error in SQL query';
-}
+else {$output['errors'][] = 'error in query';}
 
 
 
