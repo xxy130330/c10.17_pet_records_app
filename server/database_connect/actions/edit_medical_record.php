aa7bb89496f335dbc86f0fe2574d129004438578
Shoblik <shoblik@yahoo.com>
2018-01-25 22:03:42 -0800

    cleaned up edit_medical_record.php

--- server/database_connect/actions/edit_medical_record.php
+++ server/database_connect/actions/edit_medical_record.php
@@ -9,38 +9,22 @@
 if(!isset($PAGEACCESS) || $PAGEACCESS===false){
     die('NO DIRECT ACCESS ALLOWED');
 }
-
 $ID = $post['ID'];
 $title = $post['type'];
 $petID = $post['petID'];
 $recordData = $post['record_data'];
 $treatmentDate = $post['treatment_date'];
 
-//$output['errors'][] = $title;
-//$output['errors'][] = $type;
-//$output['errors'][] = $petID;
-
-
-$query = "UPDATE `medical_records` SET `title` = '$title', `type` = 'hardcoded for now', `petID` = '$petID', `record_data` = '$recordData', `treatment_date` = '$treatmentDate', `updated` = CURRENT_TIMESTAMP WHERE `ID` = $ID";
-
-$output['errors'][] = $query;
-
+$query = "UPDATE `medical_records` 
+          SET `title` = '$title', `type` = 'hardcoded for now', `petID` = '$petID', `record_data` = '$recordData', `treatment_date` = '$treatmentDate', `updated` = CURRENT_TIMESTAMP 
+          WHERE `ID` = $ID";
 $result = mysqli_query($conn, $query);
 
 if ($result) {
     if (mysqli_affected_rows($conn) > 0) {
         $output['success'] = true;
-    } else {
-        $output['errors'][] = 'no data available';
-    }
-} else {
-    $output['errors'][] = 'Error in SQL Query';
-}
-
-
-
-
-
+    } else {$output['errors'][] = 'no data available';}
+} else {$output['errors'][] = 'error in query';}
 
 
 ?>
