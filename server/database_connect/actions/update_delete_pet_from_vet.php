de598ad067449759032a0a258958b14141a5c468
Shoblik <shoblik@yahoo.com>
2018-01-25 20:25:33 -0800

    finsihed cleaning up update_delete_pet_from_vet.php

--- server/database_connect/actions/update_delete_pet_from_vet.php
+++ server/database_connect/actions/update_delete_pet_from_vet.php
@@ -6,13 +6,6 @@
  * Time: 3:28 PM
  */
 
-
-
-//$oldVetName
-//$ownerID
-//$petID
-//$refNum = null
-//
 $oldVetName = $post['oldVetName'];
 $ownerID = $post['ownerID'];
 $petID = $post['petID'];
@@ -20,15 +13,14 @@ if (!isset($refNum)) {
     $refNum = null;
 }
 
-
-
 if ($oldVetName !== 'null') {
 //Remove the this pet from the old vets account
-    $query = "SELECT `active_pets`, `ref_ID` FROM `vets` WHERE `name` = '$oldVetName'";
+    $query = "SELECT `active_pets`, `ref_ID` 
+              FROM `vets` 
+              WHERE `name` = '$oldVetName'";
     $result = mysqli_query($conn, $query);
 
     if ($result) {
-        $output['success'] = true;
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 $activePetsObj = json_decode($row['active_pets']);
@@ -37,8 +29,6 @@ if ($oldVetName !== 'null') {
                 //iterate over the array of objects
                 $count = count($activePetsObj);
                 for ($i = 0; $i < $count; $i++) {
-
-
                     if ($activePetsObj[$i]->ownerID == $ownerID) {
                         //check in case the new vet and old vet share the same name
                         if ($oldVetRefNum !== $refNum) {
@@ -46,23 +36,21 @@ if ($oldVetName !== 'null') {
                             for ($k = 0; $k < $innerCount; $k++) {
                                 //iterate over the petArray
                                 if ($activePetsObj[$i]->petID[$k] == $petID) {
-
                                     if ($innerCount === 1) {
                                         array_splice($activePetsObj, $i, 1);
-                                        $output['data'][] = 'removed owner from the vets petObj';
                                     } else {
                                         array_splice($activePetsObj[$i]->petID, $k, 1);
-                                        $output['data'][] = 'removed the pet from the vets petObj';
                                     }
                                     //check if the activePetsObj has any pets in it
                                     if (count($activePetsObj) === 0) {
                                         $activePetsObj = NULL;
                                     }
                                     $activePetsObj = json_encode($activePetsObj);
+                                    //update old vets active_pets;
+                                    $query = "UPDATE `vets` 
+                                              SET `active_pets` = '$activePetsObj' 
+                                              WHERE `ref_ID` = '$oldVetRefNum'";
 
-                                    $output['errors'][] = $activePetsObj;
-//                                        update old vets active_pets;
-                                    $query = "UPDATE `vets` SET `active_pets` = '$activePetsObj' WHERE `ref_ID` = '$oldVetRefNum'";
                                     $result = mysqli_query($conn, $query);
                                     if ($result) {
                                         if (mysqli_affected_rows($conn)) {
@@ -74,7 +62,10 @@ if ($oldVetName !== 'null') {
                                     }
                                     //if the user is only pulling their pet from a vet but not assigning them a new one
                                     if (!$vetName) {
-                                        $query = "UPDATE `pets` SET `vet` = 'No vet connected' WHERE `ID` = $petID";
+                                        $query = "UPDATE `pets` 
+                                                  SET `vet` = 'No vet connected' 
+                                                  WHERE `ID` = $petID";
+
                                         $result = mysqli_query($conn, $query);
                                         if ($result) {
                                             if (mysqli_affected_rows($conn)) {
@@ -85,23 +76,13 @@ if ($oldVetName !== 'null') {
                                             $output['success'] = false;
                                         }
                                     }
-
                                 }
                             }
-                        } else {
-                            break;
-                        }
+                        } else {break;}
                     }
                 }
             }
-        }
-        else {
-            $output['errors'][] = 'No vet found with that name';
-            $output['success'][] = false;
-        }
-    } else {
-        $output['errors'][] = 'Error in SQL Query';
-        $output['success'] = false;
-    }
+        } else {$output['success'][] = false;}
+    } else {$output['success'] = false;}
 }
 ?>
