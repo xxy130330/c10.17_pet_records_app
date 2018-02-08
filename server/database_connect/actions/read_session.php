ff34bbbf2bf8607cbdf312e28f5bc4946f0bb398
Shoblik <shoblik@yahoo.com>
2018-02-08 11:48:36 -0800

    started working on session storage, created some files that don't do anything yet

---
+++ server/database_connect/actions/read_session.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: shobl
+ * Date: 2/8/2018
+ * Time: 11:27 AM
+ */
+if(!isset($PAGEACCESS) || $PAGEACCESS===false){
+    die('NO DIRECT ACCESS ALLOWED');
+}
+
+
+
+?>
---
+++ server/database_connect/actions/update_session.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shobl
+ * Date: 2/8/2018
+ * Time: 11:31 AM
+ */
