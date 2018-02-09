7a2127f16a6e05277f59855dcc7c9ef2ed824c68
Shoblik <shoblik@yahoo.com>
2018-02-09 11:00:12 -0800

    created the two endpoints for reading and updating the session storage

---
+++ server/database_connect/actions/read_session.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shobl
+ * Date: 2/9/2018
+ * Time: 10:06 AM
+ */
+session_start();
+if(!isset($PAGEACCESS) || $PAGEACCESS===false){
+    die('NO DIRECT ACCESS ALLOWED');
+}
+$id = $post['id'];
+$auth = json_decode($_SESSION['petVetAuth']);
+$output['authorized'] = $auth->$id;
+?>
