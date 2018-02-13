96ee1a080b186aaa63aa2dbb97452d8568129490
Shoblik <shoblik@yahoo.com>
2018-02-13 13:16:14 -0800

    changed everyones profile to match mine

--- client/src/components/about_us/about_us.js
+++ client/src/components/about_us/about_us.js
@@ -23,26 +23,26 @@ const AboutUs = () => {
         </div>
 
         <div className="row teamContainer">
-          <div className="col-5">
+          <div className="profileContainer">
             <img
               src="http://clipart.printcolorcraft.com/wp-content/uploads/the-cross/jesus%20on%20the%20cross%20clipart%201.jpg"
               className="img-fluid"
             />
           </div>
-          <div className="col-7">
+          <div className="textContainer">
             <h3>this is christian</h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quod enim perspiciatis mollitia pariatur id cumque voluptatibus dolor, veniam ad fugiat repellat dolorum alias ipsam quibusdam soluta maxime magni animi?</p>
           </div>
         </div>
 
         <div className="row teamContainer">
-          <div className="col-5">
+          <div className="profileContainer">
             <img
               src="https://www.allaboutbirds.org/guide/PHOTO/LARGE/blue_jay_glamour.jpg"
               className="img-fluid"
             />
           </div>
-          <div className="col-7">
+          <div className="textContainer">
             <h3>This is Jay.</h3>
             <p>Jay worked mainly on front-end development.</p>
 
@@ -50,13 +50,13 @@ const AboutUs = () => {
         </div>
 
         <div className="row teamContainer">
-          <div className="col-5">
+          <div className="profileContainer">
             <img
               src="https://www.akingump.com/images/content/3/0/v2/30920/Carey-Christin-highres.jpg"
               className="img-fluid"
             />
           </div>
-          <div className="col-7">
+          <div className="textContainer">
             <h3>this is christin</h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quod enim perspiciatis mollitia pariatur id cumque voluptatibus dolor, veniam ad fugiat repellat dolorum alias ipsam quibusdam soluta maxime magni animi?</p>
 
@@ -64,13 +64,13 @@ const AboutUs = () => {
         </div>
 
         <div className="row teamContainer">
-          <div className="col-5">
+          <div className="profileContainer">
             <img
               src={sangwooPic}
               className="img-fluid"
             />
           </div>
-          <div className="col-7">
+          <div className="textContainer">
             <h3>Sangwoo</h3>
             <p>Sangwoo worked mainly on front-end development. Focused on a Javascript Image Cropper, Croppie.js. Utilized Croppie.js to allow users easy editing of their pet's image. He enojyed working with PetVet team as a front end developer.</p>
 
